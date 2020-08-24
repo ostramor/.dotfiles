753e5eb389c55f2a781e7187a2e319d30aff6936
ostramor <arturjaantunes@gmail.com>
2020-08-24 20:22:13 +0200

    Added some alias

--- zsh/.oh-my-zsh/custom/98_alias.zsh
+++ zsh/.oh-my-zsh/custom/98_alias.zsh
@@ -1,12 +1,14 @@
 
+alias cgrep='grep --color'
+alias la="ls -la --color"
+alias less="less -r"
+alias ll="ls -l --color"
+alias ls="ls --color"
+alias nordicvpn='openconnect --protocol=gp -u "artur.antunes@nordicsemi.no" "vpn.nordicsemi.no" --servercert pin-sha256:PsSesU2hFSBIm8XpQvzamNwpz/JiziJMudgrt9xfOs8= --csd-wrapper=/usr/lib/openconnect/hipreport.sh'
 alias q=qsubcall
 alias qr=qrsubcall
-alias vim='/usr/bin/vim -X'
-alias ls="ls --color"
-alias ll="ls -l --color"
-alias la="ls -la --color"
-alias cgrep='grep --color'
-alias less="less -r"
 alias sim='$VC_WORKSPACE/methodology/DesignKit/scripts/notools/gridsim/gridsim.py'
+alias sudo='sudo '
 alias targzUnpack='tar -xzvf'
+alias vim='/usr/bin/vim -X'
 
